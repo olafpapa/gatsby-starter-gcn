73e0afb740dd41d43029c12754f2b9a6c060fade
olafpapa <s.iwatsuru@gmail.com>
2019-08-27 11:46:21 +0900

    Change author for RSS.

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: 'https://priceless-bardeen-f24139.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
-  author: 'GCN User', // Author for RSS author segment and SEO schema
+  author: 'OlafPapa', // Author for RSS author segment and SEO schema
   authorUrl: 'https://priceless-bardeen-f24139.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@olafpapa2', // Change for Twitter Cards
   shortTitle: 'OlafPapa', // Used for App manifest e.g. Mobile Home Screen
