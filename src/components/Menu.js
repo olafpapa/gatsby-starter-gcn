c4677eeb9d38ef94cf54100fde93f383c490966c
olafpapa <s.iwatsuru@gmail.com>
2019-09-01 21:21:08 +0900

    タイトルのフォントサイズ調整。

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -31,6 +31,7 @@ const Nav = styled.nav`
   a {
     text-decoration: none;
     color: white;
+    font-size: 1.5em;
     font-weight: 600;
     transition: all 0.2s;
     border-bottom: 2px solid ${props => props.theme.colors.base};
