80a40ece5596bfa15321890dde1be44fe259bdbf
olafpapa <s.iwatsuru@gmail.com>
2019-09-03 22:48:52 +0900

    GAのTRACKING IDの環境変数名をGOOGLE_ANALYTICS_IDとした。Netlifyの環境変数にKey-Valueを設定する。

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
         head: true,
       },
     },
